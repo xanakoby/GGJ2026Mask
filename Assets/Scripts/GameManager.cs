c90d9faee7442e972a4b17b74cb751fea612ed94
xanakoby <scottmoreno6@gmail.com>
2026-01-30 16:20:42 +0100

    lilPush

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using DesignPatterns.Generics;
+using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -8,5 +9,27 @@ public class GameManager : Singleton<GameManager>
     public override void Awake()
     {
         base.Awake();
+    }
+
+    public void FreezePlayer(bool _toFreeze)
+    {
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+
+        if (_toFreeze)
+        {
+            playerInput.enabled = false;
+
+            rb.isKinematic = true;
+        }
+        else
+        {
+            playerInput.enabled = true;
+
+            rb.isKinematic = false;
+        }
+    }
+    public void ChangeScene(string _sceneName)
+    {
+        LevelManager.Instance.ChangeScene(_sceneName);
     }
 }
